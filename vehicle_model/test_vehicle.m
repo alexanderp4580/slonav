c0213cddbfe7e6defc572e4ff545293903c0d973
John Barry <jbarry510@users.noreply.github.com>
2017-02-04 20:59:35 -0800

    Steering control is WIP

--- vehicle_model/test_vehicle.m
+++ vehicle_model/test_vehicle.m
@@ -19,27 +19,31 @@ I = 0;
 D = 0;
 N = 100;
 
+% Setpoints and disturbances
+head_sp = 0.01;
+disturb_time = [0 2];
+disturb_val = [0 pi/180];
+
 %% Inputs
 % Variables that define the initial conditions and length of simulation
 
-m1_ic = 100;   % Inital motor 1 speed [rpm]
-m2_ic = 100;   % Initial motor 2 speed [rpm]
-t_start = 0;    % Simulation start time
-t_stop = 10;    % Simulation stop time
+u_start = 0.01;     % Starting speed of the vehicle
+t_start = 0;        % Simulation start time
+t_stop = 60;        % Simulation stop time
 
 %% Motor variable declarations
 % Variables are used for both motors
 
 % Motor variables
 k_motor = 167;          % [RPM/V]
-t_motor = 0.5;          % [1/sec]
+t_motor = 0.25;         % [1/sec]
 gear_ratio = 1/4.1;     % [RPM/RPM]
 
 %% Vehicle dynamics variable declarations
 % Geometry
 B = 5;             % Track width [ft]
 L = 7;             % Length between axles [ft]
-a = 4;             % Distance from center of gravity to solid axle [ft]
+a = 3.5;             % Distance from center of gravity to solid axle [ft]
 b = L - a;         % Distance from center of gravity to motors [ft]
 m = 200;           % Mass of vehicle [lb]
 I = 210000/(12^2); % Moment of inertia of vehicle [lb*ft^2]
@@ -47,7 +51,7 @@ I = 210000/(12^2); % Moment of inertia of vehicle [lb*ft^2]
 % Tire properties
 R = 4/12;   % Effective radius [ft]
 Kx = 1;     % Longitudial slip stiffness
-Ka = 0.5;   % Cornering stiffness
+Ka = 1;     % Cornering stiffness
 
 %% Runs Simulink model
 % Executes the test_vehicle_model.slx
@@ -57,7 +61,7 @@ figure(1);
 plot(pos(:,1),pos(:,2));
 xlabel('X Position');
 ylabel('Y Position');
-% ylim([-1 1]);
+ylim([-1 1]);
 figure(2);
 plot(tout,vel(:,1));
 xlabel('Time');
