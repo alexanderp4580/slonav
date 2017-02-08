c1ece43f77dd7d4e05045793e64b2b86f5ea3535
John Barry <jbarry510@gmail.com>
2017-02-07 21:40:41 -0800

    Final version of steering model for CDR

--- vehicle_model/test_vehicle.m
+++ vehicle_model/test_vehicle.m
@@ -14,11 +14,10 @@ clc
 %% Controllers
 
 % Setpoints and disturbances
-input = [500, 50];     % Waypoint [Position[ft], Velocity[ft/s]]
-look_ahead = 0.25;      % Look ahead carrot point [ft]
-disturb_time = 5;       % Time of disturbance [sec]
-disturb_val = 0;       % Intensity of disturbance [lb]
-speed_sp = 30;          % 2 * Speed of motors [%FS] 
+look_ahead = 0.25;     % Look ahead carrot point [ft]
+disturb_time = 5;      % Time of disturbance [sec]
+disturb_val = 30;      % Intensity of disturbance [lb]
+speed_sp = 30;         % 2 * Speed of motors [%FS] 
 
 % PID Controller coefficients for steering system
 steer_P = 5;
@@ -27,7 +26,7 @@ steer_D = 0.1;
 steer_N = 100;
 
 % PID Controller coefficients for speed system
-speed_P = 0.1;
+speed_P = 1;
 speed_I = 0;
 speed_D = 0;
 speed_N = 100;
@@ -37,7 +36,7 @@ speed_N = 100;
 
 u_start = 0.01;   % Starting speed of the vehicle
 t_start = 0;      % Simulation start time
-t_stop = 1000;      % Simulation stop time
+t_stop = 30;      % Simulation stop time
 
 %% Motor variable declarations
 % Variables are used for both motors
@@ -51,13 +50,13 @@ gear_ratio = 1/4.1;     % Gearing after motor [RPM/RPM]
 % Geometry
 B = 5;             % Track width [ft]
 L = 7;             % Length between axles [ft]
-a = 3.75;             % Distance from center of gravity to solid axle [ft]
+a = 3.75;          % Distance from center of gravity to solid axle [ft]
 b = L - a;         % Distance from center of gravity to motors [ft]
 m = 300/32.2;      % Mass of vehicle [lb]
 I = 210000/(12^2); % Moment of inertia of vehicle [lb*ft^2]
 
 % Tire properties
-R = 4/12;            % Effective radius [ft]
+R = 4/12;                  % Effective radius [ft]
 Kx = m*.15*180/(2*pi);     % Longitudial slip stiffness [lb/rad]
 Ka = m*.15*180/(2*pi);     % Cornering stiffness [lb/rad]
 
@@ -69,7 +68,3 @@ figure(1);
 plot(pos(:,1),pos(:,2));
 xlabel('X Position');
 ylabel('Y Position');
-figure(2);
-plot(tout,vel(:,1),tout,vel(:,2));
-xlabel('Time');
-ylabel('Tire Velocity');
