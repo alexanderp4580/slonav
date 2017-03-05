12e0e88f1de6cf92134c4fe032ad2a05db5aeca0
John Barry <jbarry510@gmail.com>
2017-03-05 13:53:35 -0800

    Added files for motor testing on scale model

---
+++ software/tests/actuator/motor_model/motor.h
@@ -0,0 +1,41 @@
+/* @file motor.h
+*
+* This file contains the driver for the X-NUCLEO-IMH04A1 dual H-Bridge motor
+* driver shield.
+*
+*/
+//------------------------------------------------------------------------------
+
+#ifndef MOTOR_H
+#define MOTOR_H
+
+#include "mbed.h"
+
+//------------------------------------------------------------------------------
+/** TODO
+*
+*   TODO
+*
+*/
+
+class MOTOR_H
+{
+
+private:
+
+protected:
+
+public:
+
+    //--------------------------------------------------------------------------
+    /** TODO
+    *
+    *   TOOD
+    *
+    */
+
+    MOTOR()
+
+}
+
+#endif
