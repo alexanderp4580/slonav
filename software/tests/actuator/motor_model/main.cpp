dc5089d37e7d6e79994d86c4085ab4280800fdce
John Barry <jbarry510@gmail.com>
2017-03-07 14:29:32 -0800

    Added PID and encoder libraries. Wrote basic test for encoders.

--- software/tests/actuator/motor_model/main.cpp
+++ software/tests/actuator/motor_model/main.cpp
@@ -1,15 +1,15 @@
-/* @file motor.h
+/* @file main.h
 *
-* This file contains the driver for the X-NUCLEO-IMH04A1 dual H-Bridge motor
-* driver shield.
+* This file contains the test code for running the motors on the sensor 
+* development platform.
 *
 */
 //------------------------------------------------------------------------------
 
 #include "mbed.h"
 #include "motor.h"
-// #include "encoder.h"
-// #include "pid.h"
+#include "QEI.h"
+#include "PID.h"
 
 //------------------------------------------------------------------------------
 
@@ -21,23 +21,33 @@
 #define ENA  PA_10
 #define ENB  PC_1
 
+// Pin definitions for encoders
+#define CHA1 PA_8
+#define CHB1 PB_10
+#define CHA2 PA_7
+#define CHB2 PB_6
+
 //------------------------------------------------------------------------------
 
 int main()
 {
     Serial ser(USBTX,USBRX);
+    QEI encoder1(CHA1, CHB1, NC, 192, QEI::X4_ENCODING);
+    QEI encoder2(CHA2, CHB2, NC, 192, QEI::X4_ENCODING);
     MOTOR motor1(1, IN2A, IN1A, ENA, true);
     MOTOR motor2(2, IN2B, IN1B, ENB, true);
     while(1)
     {
-        wait_ms(2000);
         motor1.start(20);
-        motor2.start(15);
-        wait_ms(2000);
-        motor1.setOutput(50);
-        motor2.setOutput(80);
-        wait_ms(2000);
+        motor2.start(20);
+        while (encoder1.getPulses() < 192)
+        {}
         motor1.stop();
         motor2.stop();
+        ser.printf("Encoder 1 Count: %d  ", encoder1.getPulses());
+        ser.printf("Encoder 2 Count: %d\r\n", encoder2.getPulses());
+        encoder1.reset();
+        encoder2.reset();
+        wait_ms(5000);
     }
 }
