6482674b790b53b0e5592e773bcc1c87544d9c07
Guest <guest-nznzfn@me-192-118-05>
2017-03-07 13:18:18 -0800

    added gps

--- software/tests/sensor/gps/main.cpp
+++ software/tests/sensor/gps/main.cpp
@@ -1,71 +1,28 @@
+/* Example showing how to hook up to different GPS modules (GlobalSat EM406a and Adafruit Ultimate GPSv3)
+*  to emulated serial ports at different speeds */
 #include "mbed.h"
- 
-#define LM75_REG_TEMP (0x00) // Temperature Register
-#define LM75_REG_CONF (0x01) // Configuration Register
-#define LM75_ADDR     (0x90) // LM75 address
- 
-I2C i2c(I2C_SDA, I2C_SCL);
- 
-DigitalOut myled(LED1);
- 
-Serial pc(SERIAL_TX, SERIAL_RX);
- 
-volatile char TempCelsiusDisplay[] = "+abc.d C";
- 
-int main()
-{
- 
-    char data_write[2];
-    char data_read[2];
- 
-    /* Configure the Temperature sensor device STLM75:
-    - Thermostat mode Interrupt
-    - Fault tolerance: 0
-    */
-    data_write[0] = LM75_REG_CONF;
-    data_write[1] = 0x02;
-    int status = i2c.write(LM75_ADDR, data_write, 2, 0);
-    if (status != 0) { // Error
-        while (1) {
-            myled = !myled;
-            wait(0.2);
-        }
+#include "GPS.h"
+
+/*GPS gpsSpark(D8, D7, 4800);*/
+GPS gpsAda(PA_0 ,PA_1);
+
+Serial pc(USBTX, USBRX);
+
+int main() {
+    pc.printf("Top o the morning to yah govnah USBTX: %d USBRX: %d\n\r", USBTX, USBRX);
+    gpsAda.Init();
+    
+    while (1) {  
+       pc.printf("Next Read\n\r");
+       if(gpsAda.parseData()) {
+            pc.printf("Time: %f\n\r", gpsAda.time) ;
+            pc.printf("longitude: %f\n\r", gpsAda.longitude) ;
+            pc.printf("latitude: %f\n\r", gpsAda.latitude) ;
+            pc.printf("fixtype: %f\n\r", gpsAda.fixtype) ;
+            pc.printf("satellites: %f\n\r", gpsAda.satellites) ;
+       } else {
+            pc.printf("No LOCK\r\n");
+       }   
     }
- 
-    while (1) {
-        // Read temperature register
-        data_write[0] = LM75_REG_TEMP;
-        i2c.write(LM75_ADDR, data_write, 1, 1); // no stop
-        i2c.read(LM75_ADDR, data_read, 2, 0);
- 
-        // Calculate temperature value in Celcius
-        int tempval = (int)((int)data_read[0] << 8) | data_read[1];
-        tempval >>= 7;
-        if (tempval <= 256) {
-            TempCelsiusDisplay[0] = '+';
-        } else {
-            TempCelsiusDisplay[0] = '-';
-            tempval = 512 - tempval;
-        }
- 
-        // Decimal part (0.5°C precision)
-        if (tempval & 0x01) {
-            TempCelsiusDisplay[5] = 0x05 + 0x30;
-        } else {
-            TempCelsiusDisplay[5] = 0x00 + 0x30;
-        }
- 
-        // Integer part
-        tempval >>= 1;
-        TempCelsiusDisplay[1] = (tempval / 100) + 0x30;
-        TempCelsiusDisplay[2] = ((tempval % 100) / 10) + 0x30;
-        TempCelsiusDisplay[3] = ((tempval % 100) % 10) + 0x30;
- 
-        // Display result
-        pc.printf("temp = %s\n", TempCelsiusDisplay);
-        myled = !myled;
-        wait(1.0);
-    }
- 
+    
 }
- 
