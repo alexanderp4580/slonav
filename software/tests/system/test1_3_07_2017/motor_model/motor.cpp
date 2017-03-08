43c9cfe9b2be77cde8a1356ccdb956b1432ab680
Guest <guest-nznzfn@me-192-118-05>
2017-03-07 16:45:37 -0800

    Added system test file. Works except for we have a pin conflict

---
+++ software/tests/system/test1_3_07_2017/motor_model/motor.cpp
@@ -0,0 +1,93 @@
+/* @file motor.cpp
+*
+* This file contains the driver for the X-NUCLEO-IMH04A1 dual H-Bridge motor
+* driver shield.
+*
+*/
+//------------------------------------------------------------------------------
+
+#include "motor.h"
+
+//------------------------------------------------------------------------------
+
+MOTOR::MOTOR(uint8_t motorNum, PinName forward, PinName backward, 
+	     PinName enable, bool verbose, PinName tx, PinName rx):
+ 
+	_motorNum(motorNum), _forward(forward), _backward(backward), 
+	_enable(enable,0), _verbose(verbose), _ser(tx, rx)
+{
+
+    _forward.period_us(100);
+    _forward.pulsewidth_us(0);
+    _backward.period_us(100);
+    _backward.pulsewidth_us(0);
+
+    if(_verbose == true)
+    {
+        _ser.printf("Motor %d created\r\n", motorNum);
+    }
+
+}
+
+//------------------------------------------------------------------------------
+
+void MOTOR::setOutput(int8_t percent)
+{
+
+    if(percent >= 0 && percent <= 100)
+    {
+        _backward.pulsewidth_us(0);
+        _forward.pulsewidth_us(percent);
+    }
+    else if(percent < 0 && percent >= -100)
+    {
+        _forward.pulsewidth_us(0);
+        _backward.pulsewidth_us(percent);
+    }
+    else if(percent > 100)
+    {
+        _backward.pulsewidth_us(0);
+        _forward.pulsewidth_us(100);
+    }
+    else
+    {
+        _forward.pulsewidth_us(0);
+        _backward.pulsewidth_us(100);
+    }
+
+    if(_verbose == true)
+    {
+        _ser.printf("Motor output set to %d\r\n", percent);
+    }
+
+}
+
+//------------------------------------------------------------------------------
+
+void MOTOR::stop()
+{
+
+    _forward.pulsewidth_us(0);
+    _backward.pulsewidth_us(0);
+    _enable = 0;
+
+    if(_verbose == true)
+    {
+        _ser.printf("Motor %d stopped\r\n", _motorNum);
+    }
+
+}
+
+//------------------------------------------------------------------------------
+
+void MOTOR::start(int8_t percent)
+{
+    setOutput(percent);
+    _enable = 1;
+
+    if(_verbose == true)
+    {
+        _ser.printf("Motor %d started\r\n", _motorNum);
+    }
+
+}
---
+++ software/tests/system/test1_3_07_2017/pinout.h
@@ -0,0 +1,31 @@
+/**
+ *Pinouts for the SLONav test vehical prototype
+ *TU 3/07/2017
+ *
+**/
+
+
+
+/* Nucleo Motor Cape */
+const PinName IN1A = PB_4;
+const PinName IN2A = PB_5;
+const PinName IN1B = PA_1;
+const PinName IN2B = PA_0;
+const PinName ENA  = PA_10;
+const PinName ENB  = PC_1;
+
+/* Encoders */
+const PinName CHA1 = PA_8;
+const PinName CHB1 = PB_10;
+const PinName CHA2 = PA_7;
+const PinName CHB2 = PB_6;
+
+/* IMU */
+const PinName IMDA = I2C_SDA;
+const PinName IMCL = I2C_SCL;
+
+/* GPS */
+const PinName GPTX = PA_0;
+const PinName GPRX = PA_1;
+
+
