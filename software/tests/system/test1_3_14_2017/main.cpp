0116dfd142f194907ad08e0389783e8d2ac61df3
John Barry <jbarry510@gmail.com>
2017-03-15 21:15:28 -0700

    Started adding some stuff for PID control of motors

--- software/tests/system/test1_3_14_2017/main.cpp
+++ software/tests/system/test1_3_14_2017/main.cpp
@@ -1,6 +1,6 @@
 /**
  * System integration test for SLONav test vehical
- * 3/07/2017
+ * 3/15/2017
  *
  * Note: the pinout PC_1 has a conflict.
  *       ENB = PC_1; GPRX = PC_1; 
@@ -18,6 +18,7 @@
 
 #include "motor_model/QEI.h"
 #include "motor_model/motor.h"
+#include "PID.h"
 
 
 /* Test Objects */
@@ -30,6 +31,10 @@ MOTOR MotorL(2, IN2B, IN1B, ENB, true);
 /* Encoder Objects */
 QEI EncoderL(CHA1, CHB1, NC, 192, QEI::X4_ENCODING);
 QEI EncoderR(CHA2, CHB2, NC, 192, QEI::X4_ENCODING);
+
+/* PID Objects */
+PID PIDL(1, 0, 0, 0.020);
+PID PIDR(1, 0, 0, 0.020);
 
 /* IMU Objects */
 IMU Imu(IMDA, IMCL, BNO055_G_CHIP_ADDR, true);
@@ -45,12 +50,12 @@ PwmIn E_Stop(ESTO);
 
 void setMot() {
    float throt;
-   float lr; 
+   float lr;
 
+   float inLeft;
+   float inRight;
    float left;
    float right;
-	
-   Pc.printf("B\r\n");
 
    throt = Throt.pulsewidth();
    lr = Lr.pulsewidth();
@@ -66,44 +71,60 @@ void setMot() {
    left = (1 - lr) * throt;
    right = lr * throt;
 
-   Pc.printf("Left: %d\r\n",(int)left);
-   Pc.printf("Right: %d\r\n",(int)right);
+//    PIDL.setSetPoint(inLeft);
+//    PIDR.setSetPoint(inRight);
+//    left = PIDL.compute();
+//    right = PIDR.compute();
+//    Pc.printf("Left motor output: %f\r\n", left);
+//    Pc.printf("Right motor output: %f", right);
 
    MotorL.setOutput((int)left);
    MotorR.setOutput((int)right);
 }
 
-bool encoders(int mode) {
-  if (mode) {
-	EncoderR.reset();
-	EncoderL.reset();
-  }
-  if (EncoderR.getPulses() < 15000) {
-	printf("GO\r\n");
-	return true;
-  } else {
-	printf("stop\r\n");
-	return false;
-  }
+// bool encoders(int mode) {
+//   if (mode) {
+// 	EncoderR.reset();
+// 	EncoderL.reset();
+//   }
+//   if (EncoderR.getPulses() < 15000) {
+// 	printf("GO\r\n");
+// 	return true;
+//   } else {
+// 	printf("stop\r\n");
+// 	return false;
+//   }
 
-}
+// }
 
 
 int main()
 {
-   MotorL.start(0);
-   MotorR.start(0);
+    float pticksL = 0;
+    float ticksL = 0;
+    float pticksR = 0;
+    float ticksR = 0;
 
-   encoders(1);
-   while (1) 
-   {
-      Pc.printf("a\r\n");
-      if (encoders(0)) {
-      	setMot();	
-      } else {
-	MotorL.start(0);
-	MotorR.start(0);
-      }
-   }
+    MotorL.start(0);
+    MotorR.start(0);
+    EncoderL.reset();
+    EncoderR.reset();
+
+    PIDL.setInputLimits(0, 100.0);
+    PIDR.setOutputLimits(0, 100.0);
+
+    while (1) {
+        // ticksL = (EncoderL.getPulses() - pticksL);
+        // ticksR = (EncoderR.getPulses() - pticksR);
+        // Pc.printf("Left encoder: %f \r\n", ticksL);
+        // Pc.printf("Right encoder: %f", ticksR);
+        // PIDL.setProcessValue(ticksL);
+        // PIDR.setProcessValue(ticksR);
+        // pticksL = ticksL;
+        // pticksR = ticksR;
+
+        setMot();
+        wait_ms(20);
+    }
 }
  
