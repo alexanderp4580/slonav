44952c5e77d15718ea0411b57975781e86706479
John Barry <jbarry510@gmail.com>
2017-04-06 11:38:19 -0700

    Worked on SD Card stuffff

--- software/tests/data/main.cpp
+++ software/tests/data/main.cpp
@@ -6,12 +6,43 @@
 
 #include "mbed.h"
 #include "pinout.h"
+#include "SDFileSystem.h"
 
 /* Test Objects */
 Serial Pc(USBTX,USBRX);
+// SDFileSystem sd(DO, DI, CLK, CS, "sd", CD, SDFileSystem::SWITCH_NEG_NO, 1000000);
 
 int main()
 {
-    
+    // //Mount the filesystem
+    // sd.mount();
+
+    // //Perform a write test
+    // printf("\nWriting to SD card...");
+    // FILE *fp = fopen("/sd/sdtest.txt", "w");
+    // if (fp != NULL) {
+    //     fprintf(fp, "We're writing to an SD card!");
+    //     fclose(fp);
+    //     printf("success!\n");
+    // } else {
+    //     printf("failed!\n");
+    // }
+
+    // //Perform a read test
+    // printf("Reading from SD card...");
+    // fp = fopen("/sd/sdtest.txt", "r");
+    // if (fp != NULL) {
+    //     char c = fgetc(fp);
+    //     if (c == 'W')
+    //         printf("success!\n");
+    //     else
+    //         printf("incorrect char (%c)!\n", c);
+    //     fclose(fp);
+    // } else {
+    //     printf("failed!\n");
+    // }
+
+    // //Unmount the filesystem
+    // sd.unmount();
 }
  
--- software/tests/mbed/pinout.h
+++ software/tests/mbed/pinout.h
@@ -1,6 +1,6 @@
 /**
  *Pinouts for the SLONav test vehical prototype
- *TU 3/07/2017
+ *TU 4/6/2017
  *
 **/
 
@@ -17,8 +17,8 @@ const PinName ENB  = PC_1;
 /* Encoders */
 const PinName CHA1 = PA_8;
 const PinName CHB1 = PB_10;
-const PinName CHA2 = PA_7;
-const PinName CHB2 = PB_6;
+const PinName CHA2 = PB_7; // Used to be PA_7
+const PinName CHB2 = PC_13;
 
 /* IMU */
 const PinName IMDA = I2C_SDA;
@@ -29,8 +29,17 @@ const PinName GPTX = PC_10;
 const PinName GPRX = PC_11;
 
 /* radio */
-const PinName THRO = PA_5;
+const PinName THRO = PC_7; // Used to be PA_5
 const PinName LRIN = PB_0;
 const PinName MODE = PB_3;
 const PinName ESTO = PA_9;
 
+/* SD */
+const PinName CLK = PA_5;
+const PinName DO = PA_6;
+const PinName DI = PA_7;
+const PinName CS = PA_14;
+const PinName CD = PA_15;
+
+/* Brake */
+const PinName BRK = PC_0;
