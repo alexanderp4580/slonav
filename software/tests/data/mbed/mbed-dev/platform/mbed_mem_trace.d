a4891a96066fa321fd6f8bac40f99ef7adc6a65c
John Barry <jbarry510@gmail.com>
2017-04-16 12:06:03 -0700

    Removed build files from tests and added .d files to .gitignore

--- software/tests/data/mbed/mbed-dev/platform/mbed_mem_trace.d
+++
@@ -1,4 +0,0 @@
-../mbed/mbed-dev/platform/mbed_mem_trace.o: \
- ../../mbed/mbed-dev/platform/mbed_mem_trace.c ../../mbed/mbed_config.h \
- ../../mbed/mbed-dev/platform/mbed_mem_trace.h \
- ../../mbed/mbed-dev/platform/critical.h
--- software/tests/data/mbed/mbed-dev/targets/TARGET_STM/trng_api.d
+++
@@ -1,3 +0,0 @@
-../mbed/mbed-dev/targets/TARGET_STM/trng_api.o: \
- ../../mbed/mbed-dev/targets/TARGET_STM/trng_api.c \
- ../../mbed/mbed_config.h
