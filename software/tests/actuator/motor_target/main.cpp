7cb8c1b8d48762eb7b875df984cd0396f90cc826
John Barry <jbarry510@gmail.com>
2017-05-14 02:30:20 -0700

    Updated pinout and wrote test for kelly motor controller hardware

--- software/tests/actuator/motor_target/main.cpp
+++ software/tests/actuator/motor_target/main.cpp
@@ -1,12 +1,44 @@
 #include "mbed.h"
+#include "MCP4922.h"
+#include "pinout.h"
  
-Serial pc(SERIAL_TX, SERIAL_RX);
-AnalogOut dac1();
-AnalogOut dac2();
- 
-int main()
-{
+int main() {
+    /* Serial debug output */
+    Serial pc(SERIAL_TX, SERIAL_RX);
 
+    /* Initialize motor 1 and 2 DAC objects */
+    MCP4922 motor1(MOSI2, SCLK2, CSM1);
+    MCP4922 motor2(MOSI2, SCLK2, CSM2);
+    MCP4922::MCPDAC throt, brake;
+    throt = MCP4922::DAC_A;
+    brake = MCP4922::DAC_B;
+
+    //Configure motor 1 and 2 DAC
+    motor1.referenceMode(throt, MCP4922::REF_UNBUFFERED);
+    motor1.gainMode(throt, MCP4922::GAIN_1X);
+    motor1.powerMode(throt, MCP4922::POWER_NORMAL);
+    motor1.referenceMode(brake, MCP4922::REF_UNBUFFERED);
+    motor1.gainMode(brake, MCP4922::GAIN_1X);
+    motor1.powerMode(brake, MCP4922::POWER_NORMAL);
+    motor2.referenceMode(throt, MCP4922::REF_UNBUFFERED);
+    motor2.gainMode(throt, MCP4922::GAIN_1X);
+    motor2.powerMode(throt, MCP4922::POWER_NORMAL);
+    motor2.referenceMode(brake, MCP4922::REF_UNBUFFERED);
+    motor2.gainMode(brake, MCP4922::GAIN_1X);
+    motor2.powerMode(brake, MCP4922::POWER_NORMAL);
+
+    while (1) {
+        for (float i = 0.0; i < 360.0; i += 0.1) {
+            motor1.write(throt, 0.5 * (sinf(i * 3.14159265 / 180.0) + 1));
+            wait_ms(10);
+            motor1.write(brake, 0.5 * (sinf(i * 3.14159265 / 180.0) + 1));
+            wait_ms(10);
+            motor2.write(throt, 0.5 * (sinf(i * 3.14159265 / 180.0) + 1));
+            wait_ms(10);
+            motor2.write(brake, 0.5 * (sinf(i * 3.14159265 / 180.0) + 1));
+            wait_ms(10);
+        }
+    }
  
 }
  
--- software/tests/mbed/pinout.h
+++ software/tests/mbed/pinout.h
@@ -1,45 +1,51 @@
 /**
- *Pinouts for the SLONav test vehical prototype
- *TU 4/6/2017
+ * Pinouts for the SLONav test vehical prototype
+ * TU 5/13/2017
  *
 **/
 
 
+/* Model Motor Cape */
+const PinName IN1A = PB_4; // PWM3 output
+const PinName IN2A = PB_5; // PWM3 output
+const PinName IN1B = PA_1; // PWM2 output
+const PinName IN2B = PA_0; // PWM2 output
+const PinName ENA  = PA_10; // Digital output
+const PinName ENB  = PC_1; // Digital output
 
-/* Nucleo Motor Cape */
-const PinName IN1A = PB_4;
-const PinName IN2A = PB_5;
-const PinName IN1B = PA_1;
-const PinName IN2B = PA_0;
-const PinName ENA  = PA_10;
-const PinName ENB  = PC_1;
+/* Model Brake */
+const PinName BRK = PB_6; // PWM4 output 
+
+/* Kelly Motor Controllers */
+const PinName MOSI2 = PB_15; // SPI2 MOSI
+const PinName MISO2 = PB_14; // SPI2 MISO
+const PinName SCLK2 = PB_13; // SPI2 clock
+const PinName CSM1 = PB_1; // Digital output
+const PinName CSM2 = PB_2; // Digital output
 
 /* Encoders */
-const PinName CHA1 = PA_8;
-const PinName CHB1 = PB_10;
-const PinName CHA2 = PB_7; // Used to be PA_7
-const PinName CHB2 = PC_13;
+const PinName CHA1 = PA_8; // Digital input
+const PinName CHB1 = PB_10; // Digital input
+const PinName CHA2 = PB_7; // Digital input
+const PinName CHB2 = PC_13; // Digital input
 
 /* IMU */
-const PinName IMDA = I2C_SDA;
-const PinName IMCL = I2C_SCL;
+const PinName IMDA = I2C_SDA; // I2C1 data
+const PinName IMCL = I2C_SCL; // I2C1 clock
 
 /* GPS */
-const PinName GPTX = PC_10;
-const PinName GPRX = PC_11;
+const PinName GPTX = PC_10; // Serial3 TX
+const PinName GPRX = PC_11; // Serial3 RX
 
 /* radio */
-const PinName THRO = PC_7; // Used to be PA_5
-const PinName LRIN = PB_0;
-const PinName MODE = PB_3;
-const PinName ESTO = PA_9;
+const PinName THRO = PC_7; // Digital input (PWM input)
+const PinName LRIN = PB_0; // Digital input (PWM input)
+const PinName MODE = PB_3; // Digital input (PWM input)
+const PinName ESTO = PA_9; // Digital input (PWM input)
 
 /* SD */
-const PinName CLK = PA_5;
-const PinName DO = PA_6;
-const PinName DI = PA_7;
-const PinName CS = PA_15;
-const PinName CD = PA_14;
-
-/* Brake */
-const PinName BRK = PC_0;
+const PinName CLK = PA_5; // SPI1 Clock
+const PinName DO = PA_6; // SPI1 MISO
+const PinName DI = PA_7; // SPI1 MOSI
+const PinName CS = PA_15; // Digital output
+const PinName CD = PA_14; // Digital input
