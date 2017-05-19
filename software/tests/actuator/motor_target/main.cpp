e4fb21578b3400a1ff80fcc7661bfbaf06b501f6
John Barry <jbarry510@gmail.com>
2017-05-18 17:58:40 -0700

    Tested DAC board

--- software/tests/actuator/motor_target/main.cpp
+++ software/tests/actuator/motor_target/main.cpp
@@ -7,8 +7,8 @@ int main() {
     Serial pc(SERIAL_TX, SERIAL_RX);
 
     /* Initialize motor 1 and 2 DAC objects */
-    MCP4922 motor1(MOSI2, SCLK2, CSM1, 1000000);
-    MCP4922 motor2(MOSI2, SCLK2, CSM2, 1000000);
+    MCP4922 motor1(MOSI2, SCLK2, CSM1, 100000);
+    MCP4922 motor2(MOSI2, SCLK2, CSM2, 100000);
     MCP4922::MCPDAC throt, brake;
     throt = MCP4922::DAC_A;
     brake = MCP4922::DAC_B;
@@ -28,22 +28,12 @@ int main() {
     motor2.powerMode(brake, MCP4922::POWER_NORMAL);
 
     while (1) {
-        motor1.write(throt, 0.0);
-        wait_ms(1);
-        motor1.write(brake, 0.0);
-        wait_ms(1);
-        motor1.write(throt, 1.0);
-        wait_ms(1);
-        motor1.write(brake, 1.0);
-        wait_ms(1);
-        motor2.write(throt, 0.0);
-        wait_ms(1);
-        motor2.write(brake, 0.0);
-        wait_ms(1);
-        motor2.write(throt, 1.0);
-        wait_ms(1);
-        motor2.write(brake, 1.0);
-        wait_ms(1);
+        //Generate a sine wave on DAC A
+        for (float a = 0.0; a < 360.0; a += 0.1){
+            motor1.write(throt, 0.5 * (sinf(a * 3.14159265 / 180.0) + 1));
+            wait_us(10);
+            motor1.write(brake, 0.5 * (sinf(a * 3.14159265 / 180.0) + 1));
+        }
     }
  
 }
